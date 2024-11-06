ef064d6a815d3f258e10ff7336e28901a0df7179
Pavel Balay <pavel.balai@gmail.com>
2024-11-06 23:23:52 +0300

    Implementation

--- src/sim_alg/asthash.py
+++ src/sim_alg/asthash.py
@@ -1,47 +1,36 @@
 from ast import parse
+from ast import iter_child_nodes
 
-import ast
 
-class AstHashVisitor(ast.NodeVisitor):
+class AstHashVisitor:
     def __init__(self, d, min_size = 15):
         self.mod = 1000000007
         self.d = d
         self.min_size = min_size
 
     def node_hash(self, l):
-        return sum(l)
-
-    def visit_or_default(self, node):
-        res = self.visit(node)
-
-        if res is None:
-            return 1, 301122
-
-        return res
+        return sum(l) % self.mod
 
     def visit_children(self, l):
-        return [self.visit_or_default(el) for el in l]
+        return [self.visit(el) for el in l]
 
     def save_hash(self, node, res):
         size = sum([e[0] for e in res])
         res_hash = self.node_hash([e[1] for e in res])
 
         if size >= self.min_size:
+            if (size, res_hash % self.mod) not in self.d:
+                self.d[(size, res_hash % self.mod)] = []
             self.d[(size, res_hash % self.mod)].append(node)
 
         return size, res_hash
 
-    def visit_FunctionDef(self, node):
-        return self.save_hash(node, [(1, 413)] + self.visit_children(node.body))
+    def visit(self, node):
+        children = list(iter_child_nodes(node))
+        node_hash = (1, hash(node.__class__.__name__))
+        children_hash = self.visit_children(children)
+        return self.save_hash(node, [node_hash] + children_hash)
 
-    def visit_Name(self, node):
-        return 1, 4
-
-    def visit_Call(self, node):
-        return self.save_hash(node, [(1, 423123)] + self.visit_children(node.args))
-
-    def visit_While(self, node):
-        return self.save_hash(node, [(1, 4234)] + [self.visit_or_default(node.test)] + self.visit_children(node.body))
 
 if __name__ == "__main__":
     with open('levenshtein.py', 'r') as file:
@@ -49,5 +38,3 @@ if __name__ == "__main__":
     d = dict()
     visitor = AstHashVisitor(d)
     res = visitor.visit(parse(code))
-
-    t = 8
