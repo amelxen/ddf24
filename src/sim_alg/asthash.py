01d4d3ee0d8ba81f600123233dcd852ea6a0bc2b
Vladislav Kalugin <kalugin.vlad@gmail.com>
2024-11-07 18:24:08 +0300

    add line count

--- src/sim_alg/asthash.py
+++ src/sim_alg/asthash.py
@@ -1,24 +1,37 @@
 from ast import parse
 from ast import iter_child_nodes
 
+from ast import get_source_segment
+
+def line_count(node):
+    try:
+        if node.end_lineno is None or node.end_col_offset is None:
+            return 0
+        return node.end_lineno - node.lineno + 1
+    except AttributeError:
+        return 0
 
 class AstHashVisitor:
-    def __init__(self, d, min_size = 20):
+    def __init__(self, d, min_size = 20, min_lines = 5):
         self.mod = 1000000007
         self.d = d
         self.min_size = min_size
+        self.min_lines = min_lines
 
     def node_hash(self, l):
-        return sum(l) % self.mod
+        return sum(l) % self.mod #TODO maybe change to better hash
 
     def visit_children(self, l):
         return [self.visit(el) for el in l]
 
     def save_hash(self, node, res):
-        size = sum([e[0] for e in res])
         res_hash = self.node_hash([e[1] for e in res])
 
-        if size >= self.min_size:
+        # # By subtree size
+        size = sum([e[0] for e in res])
+        lc = line_count(node)
+
+        if size >= self.min_size and lc >= self.min_lines:
             if (size, res_hash % self.mod) not in self.d:
                 self.d[(size, res_hash % self.mod)] = []
             self.d[(size, res_hash % self.mod)].append(node)
@@ -44,3 +57,4 @@ if __name__ == "__main__":
         if len(value) < 2:
             continue
         potential_clones.append(value)
+    print(potential_clones)
